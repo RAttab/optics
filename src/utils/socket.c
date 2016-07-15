4e449e0d6b0a25fd1378ede968d76ee1fe377181
Rémi Attab <remi.attab@gmail.com>
2016-07-15 13:27:54 -0400

    Improve the carbon backend tests

--- src/utils/socket.c
+++ src/utils/socket.c
@@ -43,6 +43,75 @@ int socket_stream_connect(const char *host, const char *port)
     return -1;
 }
 
+int socket_stream_listen(const char *port)
+{
+    struct addrinfo hints = {0};
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    struct addrinfo *head;
+    int err = getaddrinfo(NULL, port, &hints, &head);
+    if (err) {
+        optics_fail("unable to resolve host '0.0.0.0:%s': %s", port, gai_strerror(err));
+        return -1;
+    }
+
+    int fd = -1;
+    for (struct addrinfo *addr = head; addr; addr = addr->ai_next) {
+
+        fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+        if (fd == -1) continue;
+
+        if (!bind(fd, addr->ai_addr, addr->ai_addrlen)) {
+            if (!listen(fd, 1)) break;
+        }
+
+        close(fd);
+        fd = -1;
+    }
+
+    freeaddrinfo(head);
+
+    if (fd > 0) return fd;
+
+    optics_fail_errno("unable to connect stream socket for host '0.0.0.0:%s'", port);
+    return -1;
+}
+
+int socket_stream_accept(int fd)
+{
+    socklen_t addrlen = 0;
+    struct sockaddr addr = {0};
+
+    int accept_fd = accept4(fd, &addr, &addrlen, 0);
+    if (accept_fd >= 0) return accept_fd;
+
+    optics_fail_errno("unable to accept a socket on fd '%d'", fd);
+    return -1;
+}
+
+
+bool socket_send(int fd, size_t len, const void *data)
+{
+    ssize_t ret = send(fd, data, len, MSG_NOSIGNAL);
+    if (ret == (ssize_t) len) return true;
+
+    if (ret < 0) optics_fail_errno("unable to send announce packet");
+    else optics_fail("unable to send full announce packet: %lu != %ld\n", len, ret);
+
+    return false;
+}
+
+ssize_t socket_recv(int fd, size_t len, void *data)
+{
+    ssize_t ret = recv(fd, data, len, 0);
+    if (ret > 0) return ret;
+
+    optics_fail_errno("unable to send announce packet");
+    return -1;
+}
+
 
 // -----------------------------------------------------------------------------
 // hostname
--- test/backend_carbon_test.c
+++ test/backend_carbon_test.c
@@ -5,8 +5,13 @@
 
 #include "test.h"
 
-#include "utils/socket.h"
 #include "utils/time.h"
+#include "utils/socket.h"
+#include "utils/htable.h"
+#include "utils/buffer.h"
+
+#include <unistd.h>
+#include <pthread.h>
 
 
 // -----------------------------------------------------------------------------
@@ -24,12 +29,168 @@ void assert_carbon()
     skip();
 }
 
+struct carbon
+{
+    int listen_fd;
+    pthread_t thread;
+
+    struct buffer buffer;
+};
+
+struct carbon * carbon_start(const char *port);
+void carbon_stop(struct carbon *carbon);
+void carbon_parse(struct carbon *carbon, struct htable *table);
+
+
+// -----------------------------------------------------------------------------
+// internal
+// -----------------------------------------------------------------------------
+
+optics_test_head(backend_carbon_internal_with_source_test)
+{
+    const char *port = "12345";
+    struct carbon *carbon = carbon_start(port);
+
+    struct optics *optics = optics_create(test_name);
+    optics_set_prefix(optics, "prefix");
+    optics_set_source(optics, "source");
+
+    struct optics_lens *counter = optics_counter_alloc(optics, "counter");
+    struct optics_lens *gauge = optics_gauge_alloc(optics, "gauge");
+    struct optics_lens *dist = optics_dist_alloc(optics, "dist");
+
+    struct optics_poller *poller = optics_poller_alloc();
+    optics_poller_set_host(poller, "host");
+    optics_dump_carbon(poller, "127.0.0.1", port);
+
+    for (size_t it = 0; it < 10; ++it) {
+        optics_counter_inc(counter, 1);
+        optics_gauge_set(gauge, 1.0);
+        for (size_t i = 0; i < 100; ++i) optics_dist_record(dist, i);
+
+        if (!optics_poller_poll(poller)) optics_abort();
+
+        // sketchy way to wait for carbon to stop reading our input so we can
+        // read the result without issues.
+        nsleep(1 * 1000 * 1000);
+
+        struct htable result = {0};
+        carbon_parse(carbon, &result);
+        assert_htable_equal(&result, 0,
+                make_kv("prefix.host.source.counter", 1),
+                make_kv("prefix.host.source.gauge", 1),
+                make_kv("prefix.host.source.dist.p50", 50),
+                make_kv("prefix.host.source.dist.p90", 90),
+                make_kv("prefix.host.source.dist.p99", 99),
+                make_kv("prefix.host.source.dist.max", 99),
+                make_kv("prefix.host.source.dist.count", 100));
+
+        htable_reset(&result);
+    }
+
+    optics_poller_free(poller);
+    optics_lens_close(counter);
+    optics_lens_close(gauge);
+    optics_lens_close(dist);
+    optics_close(optics);
+    carbon_stop(carbon);
+}
+optics_test_tail()
+
+
+optics_test_head(backend_carbon_internal_without_source_test)
+{
+    const char *port = "12345";
+    struct carbon *carbon = carbon_start(port);
+
+    struct optics *optics = optics_create(test_name);
+    optics_set_prefix(optics, "prefix");
+
+    struct optics_lens *counter = optics_counter_alloc(optics, "counter");
+    struct optics_lens *gauge = optics_gauge_alloc(optics, "gauge");
+    struct optics_lens *dist = optics_dist_alloc(optics, "dist");
+
+    struct optics_poller *poller = optics_poller_alloc();
+    optics_poller_set_host(poller, "host");
+    optics_dump_carbon(poller, "127.0.0.1", port);
+
+    for (size_t it = 0; it < 10; ++it) {
+        optics_counter_inc(counter, 1);
+        optics_gauge_set(gauge, 1.0);
+        for (size_t i = 0; i < 100; ++i) optics_dist_record(dist, i);
+
+        if (!optics_poller_poll(poller)) optics_abort();
+
+        // sketchy way to wait for carbon to stop reading our input so we can
+        // read the result without issues.
+        nsleep(1 * 1000 * 1000);
+
+        struct htable result = {0};
+        carbon_parse(carbon, &result);
+        assert_htable_equal(&result, 0,
+                make_kv("prefix.host.counter", 1),
+                make_kv("prefix.host.gauge", 1),
+                make_kv("prefix.host.dist.p50", 50),
+                make_kv("prefix.host.dist.p90", 90),
+                make_kv("prefix.host.dist.p99", 99),
+                make_kv("prefix.host.dist.max", 99),
+                make_kv("prefix.host.dist.count", 100));
+
+        htable_reset(&result);
+    }
+
+    optics_poller_free(poller);
+    optics_lens_close(counter);
+    optics_lens_close(gauge);
+    optics_lens_close(dist);
+    optics_close(optics);
+    carbon_stop(carbon);
+}
+optics_test_tail()
+
 
 // -----------------------------------------------------------------------------
 // external
 // -----------------------------------------------------------------------------
 
-optics_test_head(external_test)
+optics_test_head(backend_carbon_external_with_source_test)
+{
+
+    // When turned on the test will take longer so you can have fun starting and
+    // stopping carbon. Leave off by default.
+    enum { test_disconnects = false };
+
+    if (!test_disconnects) assert_carbon();
+
+    struct optics *optics = optics_create(test_name);
+    optics_set_prefix(optics, "prefix");
+    optics_set_source(optics, "source");
+
+    struct optics_lens *lens = optics_dist_alloc(optics, "dist");
+
+    struct optics_poller *poller = optics_poller_alloc();
+    optics_poller_set_host(poller, "host");
+    optics_dump_carbon(poller, "127.0.0.1", "2003");
+
+    size_t iterations = test_disconnects ? 10 * 1000 : 100;
+
+    for (size_t t = 0; t < iterations; ++t) {
+        for (size_t i = 0; i < 10; ++i)
+            optics_dist_record(lens, i);
+
+        if (!optics_poller_poll(poller)) optics_abort();
+
+        if (test_disconnects) nsleep(1000000);
+    }
+
+    optics_poller_free(poller);
+    optics_lens_close(lens);
+    optics_close(optics);
+}
+optics_test_tail()
+
+
+optics_test_head(backend_carbon_external_without_source_test)
 {
 
     // When turned on the test will take longer so you can have fun starting and
@@ -72,8 +233,88 @@ optics_test_tail()
 int main(void)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(external_test),
+        cmocka_unit_test(backend_carbon_internal_with_source_test),
+        cmocka_unit_test(backend_carbon_internal_without_source_test),
+        cmocka_unit_test(backend_carbon_external_with_source_test),
+        cmocka_unit_test(backend_carbon_external_without_source_test),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
+}
+
+
+// -----------------------------------------------------------------------------
+// carbon
+// -----------------------------------------------------------------------------
+
+void * carbon_run(void *ctx)
+{
+    struct carbon *carbon = ctx;
+
+    int fd = socket_stream_accept(carbon->listen_fd);
+    if (fd < 0) abort();
+
+    ssize_t len;
+    char buffer[8192];
+
+    while ((len = socket_recv(fd, sizeof(buffer), buffer)) >= 0)
+        buffer_write(&carbon->buffer, buffer, len);
+
+    close(fd);
+    return NULL;
+}
+
+void carbon_parse(struct carbon *carbon, struct htable *table)
+{
+    carbon->buffer.data[carbon->buffer.len] = '\0';
+
+    char *saveptr = NULL, *line = NULL;
+    line = strtok_r(carbon->buffer.data, "\n", &saveptr);
+
+    while (line) {
+        char key[optics_name_max_len];
+        double value;
+        optics_ts_t ts;
+        sscanf(line, "%s %lf %lu", key, &value, &ts);
+
+        if (!htable_put(table, key, pun_dtoi(value)).ok) {
+            optics_fail("duplicate key detected '%s'", key);
+            optics_abort();
+        }
+
+        line = strtok_r(NULL, "\n", &saveptr);
+    }
+
+    buffer_reset(&carbon->buffer);
+}
+
+struct carbon * carbon_start(const char *port)
+{
+    struct carbon *carbon = calloc(1, sizeof(*carbon));
+
+    carbon->listen_fd = socket_stream_listen(port);
+    if (carbon->listen_fd < 0) abort();
+
+
+    int err = pthread_create(&carbon->thread, NULL, &carbon_run, carbon);
+    if (err) {
+        optics_fail_ierrno(err, "unable to start carbon thread");
+        optics_abort();
+    }
+
+    return carbon;
+}
+
+void carbon_stop(struct carbon *carbon)
+{
+    close(carbon->listen_fd);
+
+    void *ret = NULL;
+    int err = pthread_join(carbon->thread, &ret);
+    if (err) {
+        optics_fail_ierrno(err, "unable to join carbon thread");
+        optics_abort();
+    }
+
+    free(carbon);
 }
